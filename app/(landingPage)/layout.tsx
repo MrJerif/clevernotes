b23a6a9318855be8efbf21c31563b402b1ebc991
MrJerif <danish1402111@gmail.com>
2025-02-03 20:06:29 +0530

    First commit

--- app/(landingPage)/layout.tsx
+++ app/(landingPage)/layout.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Navbar } from './_components/navbar';
-import { Metadata } from 'next';
+// import { Metadata } from 'next';
 
-export const meta: Metadata = {
-  metadataBase: new URL(`${process.env.NEXT_PUBLIC_BASE_URL}`),
-  keywords: ['CleverNotes', 'Notes', 'Organize', 'Note App', 'Note taking', 'Ai', 'Gemini', 'Web App', 'Project',
-      'Markdown', 'readme', 'pdf', 'document', 'note',],
-  title: 'CleverNotes',
-  description: 'Create notes, files in markdown, share via unique links or download. CleverNotes is an AI-powered note-taking app that helps you write better notes.',
-}
+// export const meta: Metadata = {
+//   metadataBase: new URL(`${process.env.NEXT_PUBLIC_BASE_URL}`),
+//   keywords: ['CleverNotes', 'Notes', 'Organize', 'Note App', 'Note taking', 'Ai', 'Gemini', 'Web App', 'Project',
+//       'Markdown', 'readme', 'pdf', 'document', 'note',],
+//   title: 'CleverNotes',
+//   description: 'Create notes, files in markdown, share via unique links or download. CleverNotes is an AI-powered note-taking app that helps you write better notes.',
+// }
 
 const LandingPageLayout = ({
   children
